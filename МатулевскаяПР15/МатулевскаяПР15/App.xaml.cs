4119f827305770d3fdb38add9cfdeea249d055b9
Кристина Матулевская <matulevskaakristina@gmail.com>
2022-05-22 19:58:07 +0300

    Добавьте файлы проекта.

---
+++ МатулевскаяПР15/МатулевскаяПР15/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+
+namespace МатулевскаяПР15
+{
+    public partial class App : Application
+         
+    {
+        static TimeDataBase dataBase;
+        internal static TimeDataBase DataBase
+        {
+            get
+            {
+                if (dataBase == null)
+                {
+                    dataBase = new TimeDataBase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Times.db3"));
+                }
+                return dataBase;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
